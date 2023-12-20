4e14c96ba43608d218d0f7e56406afa8e4bc704c
Hassan ElMasry <hassansherifelmasry@gmail.com>
2023-12-20 20:22:31 +0100

    Add files via upload

--- App.java
+++ App.java
@@ -1,137 +1,77 @@
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class App {
 
-    public static class Stopwatch{
-        private final BigDecimal start;
-        public Stopwatch() {
-            start = BigDecimal.valueOf(System.currentTimeMillis());
-        }
-        public BigDecimal elapsedTime() {
-            BigDecimal now = BigDecimal.valueOf(System.currentTimeMillis());
-            BigDecimal temp = now.subtract(start);
-            BigDecimal divider = BigDecimal.valueOf(1000.0);
-            return temp.divide(divider);
-        }
-    }
-    public static void main(String[] args) throws Exception {
-        //Stopwatch watch = new Stopwatch();
-        //System.out.println(powerDivNConq(5, 1));
-        int [] a = {1,4,2,3,-1,6,7,-2,11,-6};
-        int[] ab = mergeSortArray(a,0, a.length - 1);
-        for(int i = 0; i < ab.length; i++){
-            System.out.print(ab[i] + "   ");
-        }
-        System.out.println();
-        List<int[]> pair = findPairs(a, 5);
-        for(int[] p : pair){
-            System.out.println(p[0] + "   " + p[1]);
-        }
-        //System.out.println(binarySearch(ab,22,0, a.length));
-        //BigDecimal time = watch.elapsedTime();
-        //System.out.println(time);
+    static class res {
+        String doneX;
+        String doneY;
+        double score;
+        public res(String doneX, String doneY, double score){
+            this.doneX = doneX;
+            this.doneY = doneY;
+            this.score = score;
+            double threshold = 1e-10;
+            if (Math.abs(this.score) < threshold) {
+                this.score = 0.0;
+            }
+        }   
     }
 
-    public static BigInteger powerStandard(int a, int n){
-        BigInteger result = BigInteger.valueOf(1);
-        BigInteger bigA = BigInteger.valueOf(a);
-        while(n > 0){
-            result = result.multiply(bigA);
-            n--;
+    public static res allignment(String x, String y, Map< Character, Map<Character, Double>> matrix) {
+        double[][] temp = new double[x.length() + 1][y.length() + 1];
+        for (int j = 1; j <= y.length(); j++) {
+            temp[0][j] = temp[0][j - 1] + matrix.get('-').get(y.charAt(j - 1));
         }
-        return result;
-    }
-
-    public static BigInteger powerDivNConq(int a, int n){
-        BigInteger result = BigInteger.valueOf(1);
-        BigInteger bigA = BigInteger.valueOf(a);
-        if(n == 0){
-            return result;
+        for (int i = 1; i <= x.length(); i++) {
+            temp[i][0] = temp[i - 1][0] + matrix.get(x.charAt(i - 1)).get('-');
         }
-        else if(n == 1){
-            return bigA;
+        for(int i = 1; i <= x.length(); i++){
+            for(int j = 1; j <= y.length(); j++){
+                double match = temp[i-1][j-1] + matrix.get(x.charAt(i-1)).get(y.charAt(j-1));
+                double delete = temp[i-1][j] + matrix.get(x.charAt(i-1)).get('-');
+                double insert = temp[i][j-1] + matrix.get('-').get(y.charAt(j-1));
+                temp[i][j] = Math.max(match, Math.max(delete, insert));
+            }
         }
-        else{
-            if(n % 2 == 0){
-                result = powerDivNConq(a, n/2);
-                return result.multiply(result);
+        String doneX = "";
+        String doneY = "";
+        int i = x.length();
+        int j = y.length();
+        while(i > 0 || j > 0){
+            if(i > 0 && j > 0 && temp[i][j] == temp[i - 1][j - 1] + matrix.get(x.charAt(i-1)).get(y.charAt(j-1))){
+                doneX = x.charAt(i-1) + doneX;
+                doneY = y.charAt(j-1) + doneY;
+                i--;
+                j--;
+            }
+            else if(i > 0 && temp[i][j] == temp[i - 1][j] + matrix.get(x.charAt(i-1)).get('-')){
+                doneX = x.charAt(i-1) + doneX;
+                doneY = '-' + doneY;
+                i--;
             }
             else{
-                result = powerDivNConq(a, (n-1)/2);
-                return result.multiply(result).multiply(bigA);
+                doneX = '-' + doneX;
+                doneY = y.charAt(j-1) + doneY;
+                j--;
             }
         }
+        return new res(doneX, doneY, temp[x.length()][y.length()]);
     }
-
-    public static int[] mergeSortArray(int[] array, int f, int l ){
-        // if the array is only 1 element
-        if (f == l){
-            int [] r = new int[1];
-            r[0] = array[f];
-            return r;
-        }
-        // split the array into 2 parts
-        int mid = (f + l)/2;
-        // sort the left side with recursion
-        int[] left = mergeSortArray(array, f, mid);
-        // sort the right side with recursion
-        int[] right = mergeSortArray(array, mid + 1, l);
-        // merge the 2 arrays ma3 ba3d when its done sorting
-        int [] r = mergeSorted(left, right);
-        return r;
+    public static void main(String[] args) throws Exception {
+        String x = "TCCCAGTTATGTCAGGGGACACGAGCATGCAGAGAC";
+        String y = "AATTGCCGCCGTCGTTTTCAGCAGTTATGTCAGATC";
+        //String x = "ATGCC";
+        //String y = "TACGCA";
+        Map< Character, Map<Character, Double>> matrix = new HashMap<>();
+        matrix.put('A', new HashMap<>(Map.of('A', 1.0, 'G', -0.8, 'T', -0.2, 'C', -2.3, '-', -0.6)));
+        matrix.put('G', new HashMap<>(Map.of('A', -0.8, 'G', 1.0, 'T', -1.1, 'C', -0.7, '-', -1.5)));
+        matrix.put('T', new HashMap<>(Map.of('A', -0.2, 'G', -1.1, 'T', 1.0, 'C', -0.5, '-', -0.9)));
+        matrix.put('C', new HashMap<>(Map.of('A', -2.3, 'G', -0.7, 'T', -0.5, 'C', 1.0, '-', -1.0)));
+        matrix.put('-', new HashMap<>(Map.of('A', -0.6, 'G', -1.5, 'T', -0.9, 'C', -1.0, '-', Double.NaN)));
+        res result = allignment(x, y, matrix);
+        System.out.println(result.doneX);
+        System.out.println(result.doneY);
+        System.out.println(result.score);
     }
-
-    public static int[] mergeSorted(int [] a, int [] b){
-        int[] result = new int[a.length + b.length];
-        int rstart = 0;
-        int acount = 0;
-        int bcount = 0;
-        while(acount < a.length && bcount < b.length){
-            if(a[acount] <= b[bcount]){
-                result[rstart++] = a[acount++];
-            }else{
-                result[rstart++] = b[bcount++];
-            }
-        }
-        while(acount < a.length){
-            result[rstart++] = a[acount++];
-        }
-        while(bcount < b.length){
-            result[rstart++] = b[bcount++];
-        }
-        return result;
-    }
-
-    public static boolean binarySearch(int[] array , int key, int low, int high){
-        int mid = low + ((high - low) / 2);
-        if(low > high){
-            return false;
-        }
-        if(array[mid] == key){
-            return true;
-        }else if(array[mid] > key){
-            return binarySearch(array, key, low , mid - 1);
-        }else{
-            return binarySearch(array, key, mid + 1, high);
-        }
-
-    }
-
-
-    public static List<int[]> findPairs(int[] array, int sum){
-        array = mergeSortArray(array,0, array.length - 1);
-        List<int[]> pairs = new ArrayList<>();
-        for(int i = 0; i < array.length; i++){
-            int remainder = sum - array[i];
-            if(binarySearch(array,remainder,i + 1, array.length - 1)){
-                pairs.add(new int[]{array[i], remainder});
-            }
-        }
-        return pairs;
-    }
-
 }
-
